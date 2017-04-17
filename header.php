90b04285f568fd21d005eb854b34294b7a4b8f6f
Timothy Law <timlyg@yahoo.com>
2017-04-16 20:26:21 -0400

    test

--- header.php
+++ header.php
@@ -6,24 +6,15 @@
 
 #left {
     float:left;
-    width:100px;
-    height: 20px;
-    background: #ff0000;
 }
 
 #center {
     display: inline-block;
     margin:0 auto;
-    width:100px;
-    height: 20px;
-    background: #00ff00;
 }
 
 #right {
     float:right;
-    width:100px;
-    height: 20px;
-    background: #0000ff;
 }
 </style>
 
